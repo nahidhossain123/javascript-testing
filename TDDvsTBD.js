6651b3c0bad701a791884a876654d1a76a9b7537
Nahid Hossain <nahidhossain351@gmail.com>
2025-02-24 18:02:43 +0600

    TDD vs TBD

---
+++ TDDvsTBD.js
@@ -0,0 +1,15 @@
+//TDD vs BDD
+
+//Test Driven Development
+//mainly focusing on unit testing 
+//write a test case then create or implement 
+
+//Behaviour Driven Development
+//as a scenario
+//a user want to login and access account 
+//scenario: success login
+//given the user is on login page
+//when user enter the credentials 
+//then user should redirect to dashboard 
+
+//Cucumber js
